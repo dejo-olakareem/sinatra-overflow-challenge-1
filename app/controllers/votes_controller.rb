e5bde47e18cb48588842d8151bdf8f7110c36e79
Amber Nash & oladejo olakareem & Eric O'Rear & Joe Viveiros & Matthew Weingarten <pair+annnash88.dejo-olakareem.ejoreo.jviveiros.kalmaroso@devbootcamp.com>
2017-01-13 10:35:35 -0800

    Vote stuff

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,15 @@
+post '/questions/:id/upvote' do
+  #
+  # if user?
+  #   Vote.new
+  # end
+
+end
+
+post '/questions/:id/downvote' do
+
+  "downvote"
+  # <!-- <%= question.votes.map!{|vote| vote.valgit ue}.inject(:+) %> -->
+
+
+end
