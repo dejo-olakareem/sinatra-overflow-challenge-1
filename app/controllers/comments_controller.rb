7b68dc2d5019f7ab21241fb9e905d7e45971578f
Amber Nash & oladejo olakareem & Eric O'Rear & Joe Viveiros & Matthew Weingarten <pair+annnash88.dejo-olakareem.ejoreo.jviveiros.kalmaroso@devbootcamp.com>
2017-01-13 15:43:43 -0800

    Edits routes to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,22 +4,27 @@ get '/comments' do
 end
 
 
-get '/comments/new' do
-
+get '/:commentable_id/comments/new' do
+  @commentable_id = params[:commentable_id]
   erb :'/comments/_new.html'
 end
 
 
-post '/comments' do
+post '/:commentable_id/comments' do
   #how do I get the commentable_type info onto the form?
   p "I made it to the server!!!!!"
-  @comment = Comment.create(body: params[:comment][:body])
+  @comment = Comment.new(body: params[:comment][:body], commentable_type: params[:comment][:class].constantize, commentable_id: params[:commentable_id],user_id: current_user)
 
+  if @comment.save
+    redirect "/"
+  else
+    redirect "/comments/#{params[:commentable_id]}/new"
+  end
 end
 
 
 get '/comments/:id/edit' do
-
+  @id = params[:id]
   @comment = Comment.find(params[:id])
 
   erb :'comments/_edit.html' #show edit comment view
@@ -48,5 +53,3 @@ delete '/comments/:id' do
 
   redirect '/comments' #or wherever we want
 end
-
-
