bfa5c8f3a59c29479c84a11366a608c69a6a676f
Amber Nash & oladejo olakareem & Eric O'Rear & Joe Viveiros & Matthew Weingarten <pair+annnash88.dejo-olakareem.ejoreo.jviveiros.kalmaroso@devbootcamp.com>
2017-01-13 16:59:58 -0800

    AJAX the votes

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  increaseVoteCount();
 });
+
+var increaseVoteCount = function() {
+  var listeningSection = $('#container');
+  listeningSection.on("click", '.upvote', function(e){
+    e.preventDefault();
+    console.log("I been clicked!")
+    var url = $(this).attr('action')
+    var method = $(this).attr('method')
+    var data = $(this).serialize()
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    }).done(function(response){
+      console.log($(this).siblings('span .vote_value'))
+      $(this).siblings('span .vote_value').first().html(response)
+    })
+  })
+};
