845c539cecf39e49ff1fb0252d20f5477acc989f
Eric O'Rear <ejorear@gmail.com>
2017-01-13 16:51:44 -0800

    Adds comments and voting on comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,7 +5,6 @@ post '/upvotes/:id/new' do
 
   if user?
   @vote = Vote.new(value: 1, voteable_type: @class_name, voteable_id: params[:id], user_id: current_user)
-
    if vote_user_include(@class_name.find(params[:id])) == false
      @vote.save
    end
